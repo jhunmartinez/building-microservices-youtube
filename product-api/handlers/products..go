4712f303de5b265f013f50dc8c53df07f6029d09
Martinez, Jhun Rhenz <jhunmartinez1@gmail.com>
2021-12-14 16:47:47 -0500

    changes in data and handler's product.go

--- product-api/handlers/products..go
+++ product-api/handlers/products..go
@@ -26,6 +26,16 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		p.addProducts(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		// expect the id in the URI
+		p := r.URL.Path
+	}
+
 	// catch all
 	// if no method is satisfied return an error
 	rw.WriteHeader(http.StatusMethodNotAllowed)
@@ -43,4 +53,18 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Products")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
+
 }
