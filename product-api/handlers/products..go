e7ce27f1fe632f415b9f16a67bd543272f8e2224
Martinez, Jhun Rhenz <jhunmartinez1@gmail.com>
2021-12-15 20:20:58 -0500

    updated products.go for PUT requests
    products.go in data and handler

--- product-api/handlers/products..go
+++ product-api/handlers/products..go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strconv"
 
 	"github.com/nicholasjackson/building-microservices-youtube/product-api/data"
 )
@@ -33,10 +34,34 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == http.MethodPut {
+		p.l.Println("PUT", r.URL.Path)
 		// expect the id in the URI
-		r := regexp.MustCompile(`/([0-9]+)`)
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
 
-		p := r.URL.Path
+		if len(g) != 1 {
+			p.l.Println("Invalid URI more than one id")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI more than one capture group")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URI unable to convert to numer", idString)
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+
+		}
+
+		p.updateProducts(id, rw, r)
+		return
 
 	}
 
@@ -71,4 +96,25 @@ func (p *Products) addProducts(rw http.ResponseWriter, r *http.Request) {
 	p.l.Printf("Prod: %#v", prod)
 	data.AddProduct(prod)
 
+}
+
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Products")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
 }
